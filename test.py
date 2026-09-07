c62bd3fbc9c1e7fef603234259aa64fdcdb0b4e0
Zerberstian <zerberstian681@gmail.com>
2026-09-07 13:56:53 +0200

    refactor: API key reading and output in test script

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ file_path = Path(__file__).parent / "x-api_key.txt"
 def read_text_file(path):
     with open(path, "r", encoding="utf-8") as f:
         return f.read().strip()
-print(f"\n Key:\n")
-print(read_text_file(file_path))
+
+x_api_key = read_text_file(file_path)
+print(f"\nKey: {x_api_key}\n")
