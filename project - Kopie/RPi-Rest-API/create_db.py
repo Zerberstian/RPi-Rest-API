dd0b1662410006f9aa81028f5f071269b8684a47
Zerberstian <zerberstian681@gmail.com>
2026-09-09 16:07:42 +0200

    refactor: restructured Project

--- project - Kopie/RPi-Rest-API/create_db.py
+++
@@ -1,4 +0,0 @@
-from rest_api import db, app
-
-with app.app_context():
-    db.create_all()
--- project/run.py
+++ project/run.py
@@ -7,10 +7,10 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 api = subprocess.Popen([
 os.path.join(base_dir, "RPi-Rest-API", "venv", "bin", "python"),
-os.path.join(base_dir,"RPi-Rest-API", "rest_apib.py")])
+os.path.join(base_dir,"RPi-Rest-API", "rest_api.py")])
 time.sleep(10)
 
-sensor = subprocess.Popen(["python3", os.path.join(base_dir, "sensorb.py")])
+sensor = subprocess.Popen(["python3", os.path.join(base_dir, "sensor.py")])
 
 abfrage = subprocess.Popen(["python3", os.path.join(base_dir, "anfrage.py")])
 
